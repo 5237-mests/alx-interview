272eb3a42f916da44b7a74c5d78ff2c0d4289762
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-12-20 19:19:39 +0300

    again optimize code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,12 +2,13 @@ def pascal_triangle(n):
     bigarray = []
     if (n <= 0):
         bigarray.append([])
-    for i in range(n):
-        tmp = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                tmp.append(1)
-            else:
-                tmp.append(bigarray[i - 1][j - 1] + bigarray[i - 1][j])
-        bigarray.append(tmp)
+    else:
+        for i in range(n):
+            tmp = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    tmp.append(1)
+                else:
+                    tmp.append(bigarray[i - 1][j - 1] + bigarray[i - 1][j])
+            bigarray.append(tmp)
     return bigarray
