d4aa6f61bf6c40a10e2d04ef6e33748d6b1c298a
5237-mests <msfnmulgeta@gmail.com>
2023-02-07 10:25:51 +0300

    edited

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,26 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process');
 
 const inputNum = process.argv;
 
 const url = `https://swapi-api.alx-tools.com/api/films/${inputNum[2]}/`;
 
-request(url, function (err, response, body) {
-  if (err) throw console.error(err);
-  const newbody = JSON.parse(body);
-  const people = newbody.characters;
-  for (const actor of people) {
-    request(actor, function (err, res, body2) {
-      if (err) throw console.error(err);
-      const newbody2 = JSON.parse(body2);
-      console.log(newbody2.name);
-    });
-  }
-});
+if (inputNum.length > 2) {
+  request(url, function (err, response, body) {
+    if (err) throw console.error(err);
+    const charactersUrl = JSON.parse(body).characters;
+    const charactersName = charactersUrl.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (err, _, responseBody) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(JSON.parse(responseBody).name);
+        });
+      })
+    );
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErrors => console.log(allErrors));
+  });
+}
