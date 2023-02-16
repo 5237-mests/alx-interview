7e3e10cb235ed369774d5dd5211e9371c66660e3
5237-mests <msfnmulgeta@gmail.com>
2023-02-16 18:48:08 +0300

    second

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,26 +4,26 @@
 
 
 def rotate_2d_matrix(matrix):
-    """an n x n 2D matrix, rotate it 90 degrees clockwise.
-    """
+    """rotate matrix """
     if type(matrix) != list:
         return
     if len(matrix) <= 0:
         return
     if not all(map(lambda x: type(x) == list, matrix)):
         return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
+    column = len(matrix[0])
+    if not all(map(lambda x: len(x) == column, matrix)):
         return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+
+    deepcopied_matrix = matrix.copy()
+    for k in range(len(deepcopied_matrix)):
+        deepcopied_matrix[k] = deepcopied_matrix[k].copy()
+
+    new_matrix = matrix
+    y = len(deepcopied_matrix)
+    for i in range(len(deepcopied_matrix)):
+        y = y - 1
+        x = 0
+        for j in deepcopied_matrix[i]:
+            new_matrix[x][y] = j
+            x = x + 1
