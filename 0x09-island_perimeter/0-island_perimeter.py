0fe97f8ad1fc9770e8f276ff00dce9acfe3ad8b9
5237-mests <msfnmulgeta@gmail.com>
2023-02-28 12:55:48 +0300

    fourth

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,25 +10,12 @@ def island_perimeter(grid):
     row = len(grid)
     col = len(grid[0])
     perimeter = 0
-
     for i in range(row):
         for j in range(col):
-            if i == 0:
-                if grid[i][j] != 0:
-                    return 0
-            if j == 0:
-                if grid[i][j] != 0:
-                    return 0
-            if i == row - 1:
-                if grid[i][j] != 0:
-                    return 0
-            if j == col - 1:
-                if grid[i][j] != 0:
-                    return 0
-            if grid[i][j] == 1:
+            if grid[i][j]:
                 perimeter += 4
-                if grid[i][j + 1] == 1:
+                if grid[i][j + 1] and j < col - 1:
                     perimeter -= 2
-                if grid[i + 1][j] == 1:
+                if grid[i + 1][j] and i < row - 1:
                     perimeter -= 2
     return perimeter
